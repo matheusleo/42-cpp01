b6986e87446f9a88821753da5d0213981bd154fe
matheusleo <matheusleotrab@gmail.com>
2024-06-04 00:23:37 -0300

    feat: ex02

---
+++ ex02/src/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mleonard <mleonard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 00:15:30 by mleonard          #+#    #+#             */
+/*   Updated: 2024/06/04 00:22:12 by mleonard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+
+    std::cout << "Address from str: " << &str << std::endl;
+    std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Value from str: " << str << std::endl;
+    std::cout << "Value from stringPTR: " << stringPTR << std::endl;
+    std::cout << "Value from stringREF: " << stringREF << std::endl;
+}
